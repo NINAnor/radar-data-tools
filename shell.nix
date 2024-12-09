d7fb733d24a635918914fb903c483a4916951974
Niccolò Cantù <niccolo.cantu@nina.no>
2024-12-09 15:48:19 +0100

    pipeline for extracting data from dumps

--- shell.nix
+++ shell.nix
@@ -4,9 +4,26 @@ with import <nixpkgs> {
 let
   unstable = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/tarball/b69de56fac8c2b6f8fd27f2eca01dcda8e0a4221) {};
 in pkgs.mkShell rec {
+  name = "impurePythonEnv";
+  venvDir = "./.venv";
   buildInputs = [
+    unstable.python312Packages.venvShellHook
     unstable.parallel
     unstable.gdal
     unstable.duckdb
+    unstable.python312
   ];
+
+  # Run this command, only after creating the virtual environment
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+    pip install -r requirements.txt
+  '';
+
+  # Now we can execute any commands within the virtual environment.
+  # This is optional and can be left out to run pip manually.
+  postShellHook = ''
+    # allow pip to install wheels
+    unset SOURCE_DATE_EPOCH
+  '';
 }
